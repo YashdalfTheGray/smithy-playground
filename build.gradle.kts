ca546214b00f63b58fcdb9f1838fd2a1b25009ae
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-24 01:16:12 -0800

    added the plugin configuration and dependencies for smithy

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ plugins {
     id("software.amazon.smithy").version("0.4.2")
 }
 
+configure<software.amazon.smithy.gradle.SmithyExtension> {
+    projection = "source"
+}
+
 repositories {
     // Use jcenter for resolving your dependencies.
     // You can declare any Maven/Ivy/file repository here.
@@ -21,6 +25,12 @@ repositories {
 dependencies {
     // Use the Kotlin JDK 8 standard library
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Pull in all the Smithy stuff
+    implementation("software.amazon.smithy:smithy-model:0.9.7")
+    implementation("software.amazon.smithy:smithy-openapi:0.9.7")
+    implementation("software.amazon.smithy:smithy-aws-traits:0.9.7")
+    implementation("software.amazon.smithy:smithy-cli:0.9.7")
 
     // Use the Kotlin test library
     testImplementation("org.jetbrains.kotlin:kotlin-test")
